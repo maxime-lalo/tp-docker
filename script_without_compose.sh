6b7bd099c9eef58ef85bdd4b015af5dc7e785162
Maxime <maxime.lalo.pro@gmail.com>
2021-12-15 16:01:40 +0100

    Initial commit

---
+++ script_without_compose.sh
@@ -0,0 +1,16 @@
+# Creating the networks
+sudo docker network create -d bridge --subnet=172.1.1.0/24 MyBridgedNetwork
+sudo docker network create -d bridge --subnet=172.2.2.0/24 BCNetwork
+
+# Part of the web server
+sudo docker run -d -it --network MyBridgedNetwork --name web nginx 
+
+# Part for mongo
+sudo docker volume create bdd
+sudo docker run -d -it --network MyBridgedNetwork --mount source=bdd,destination=/bdd --name bdd mongo:4.2.2
+
+# Part for the ether node
+sudo docker volume create blockchain
+sudo docker build -t geth ./geth
+sudo docker run -d -it --network BCNetwork -mount source=blockchain,destination=/blockchain --name geth geth
+
